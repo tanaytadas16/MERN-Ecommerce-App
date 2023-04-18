5f84bce259b34ca6cf2e50f4f7785a4b8cecaf17
Tanay Tadas <91279967+tanaytadas16@users.noreply.github.com>
2023-04-17 20:02:03 -0500

    Folder Structure Created and images added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout";
+import Home from "./Components/Home";
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={Home} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
